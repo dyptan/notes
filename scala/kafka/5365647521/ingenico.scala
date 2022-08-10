5b6107a0f8a32e0ac77d44a45b99652ee10cb3c9
Ivan Dyptan <ivdyptan@gmail.com>
2022-08-10 12:48:48 +0200

    Update ingenico.scala

--- scala/kafka/5365647521/ingenico.scala
+++ scala/kafka/5365647521/ingenico.scala
@@ -8,7 +8,7 @@ import java.util.{Collections, Properties, UUID}
 import scala.collection.JavaConversions.iterableAsScalaIterable
 import scala.collection.JavaConverters.{asScalaIteratorConverter, mapAsScalaMapConverter}
 
-0 until 1000 foreach { i =>
+0 until 100 foreach { i =>
 
 val catalogStream = "/user/mapr/ingenico"
 val pollTimeout = 1000
@@ -23,7 +23,7 @@ properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId)
 properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
 properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[ByteArrayDeserializer].getName)
 properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
-properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
+properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false")
 properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100")
 
 val bytesStringKafkaConsumer = new KafkaConsumer[Bytes, String](properties)
@@ -31,29 +31,26 @@ val bytesStringKafkaConsumer = new KafkaConsumer[Bytes, String](properties)
 bytesStringKafkaConsumer.subscribe(Collections.singletonList(s"$catalogStream:$topic"))
 val newCatalog = new ConcurrentHashMap[Long, Bytes]
 
-var done = false
-while ( {
-!done
-}) {
-val records: ConsumerRecords[Bytes, String] = bytesStringKafkaConsumer.poll(pollTimeout)
-val list = records.toList
-if (list.isEmpty) { // register new catalog
-println(s"iteration: $i, Records are empty, records: ${newCatalog.entrySet().iterator().asScala.map(r => s"key: ${r.getKey} off: ${r.getValue}").mkString("\n")}")
-val newCatalogSize = newCatalog.size()
-println(s"iteration: $i, Catalog size: $newCatalogSize")
 val topicPartition = new TopicPartition(s"$catalogStream:$topic", 0)
-println(s"iteration: $i, Position: ${bytesStringKafkaConsumer.position(topicPartition)}")
+// println(s"iteration: $i, Position: ${bytesStringKafkaConsumer.position(topicPartition)}")
+
 val endOffsets = bytesStringKafkaConsumer.endOffsets(Collections.singletonList(topicPartition))
 val endOffset = endOffsets.asScala(topicPartition)
-println(s"iteration: $i, end offset: $endOffsets")
-if (newCatalogSize != endOffset) throw new RuntimeException("wrong size!!!!! AAAARGH")
 
-done = true
+while (newCatalog.size < endOffset) {
+
+    val records: ConsumerRecords[Bytes, String] = bytesStringKafkaConsumer.poll(pollTimeout)
+    val list = records.toList
+
+
+    list.foreach(r => newCatalog.put(r.offset(), r.key()))
+    println(s"iteration: $i, Records get, records: ${list.map(r => s"key: ${r.key()} offset: ${r.offset()}").mkString("\n")}")
+    
+    println(s"iteration: $i, end offset: $endOffsets")
+
+}
+
+if (newCatalog.size != endOffset) throw new RuntimeException("wrong size!!!!! AAAARGH")
 bytesStringKafkaConsumer.close()
 
-} else {
-list.foreach(r => newCatalog.put(r.offset(), r.key()))
-println(s"iteration: $i, Records get, records: ${list.map(r => s"key: ${r.key()} offset: ${r.offset()}").mkString("\n")}")
-}
-}
 }
