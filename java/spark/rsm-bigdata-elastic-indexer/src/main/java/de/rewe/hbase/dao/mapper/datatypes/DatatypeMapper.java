4bbcb0bfec0a26553e836434421c175304df0b90
Ivan Dyptan <ivdyptan@gmail.com>
2023-02-22 17:27:55 +0100

    Delete java/spark/rsm-bigdata-elastic-indexer directory

--- java/spark/rsm-bigdata-elastic-indexer/src/main/java/de/rewe/hbase/dao/mapper/datatypes/DatatypeMapper.java
+++
@@ -1,9 +0,0 @@
-package de.rewe.hbase.dao.mapper.datatypes;
-
-/**
- * Interface for all classes to translate an object into a byte array and back to the object
- * 
- */
-public interface DatatypeMapper {
-
-}
--- java/spark/rsm-bigdata-elastic-indexer/src/test/resources/script/jobstatus_elastic_indexer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-scriptPath=${0%/*}
-source $scriptPath/set_volume_infos.sh
-
-$MAPR_SCRIPTS_HOME/v1/user/login.sh -v $VOLUME_HOME
-
-$MAPR_SCRIPTS_HOME/v1/spark/jobstatus.sh -v $VOLUME_HOME -l elasticIndexer
